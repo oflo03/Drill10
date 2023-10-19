ff2dc7cc7476cf52d4a18dd84e1b01c25354247e
oflo03 <samleebw@naver.com>
2023-10-19 12:33:02 +0900

    Drill 10 제출 (2022184027 이병욱)

---
+++ game_world.py
@@ -0,0 +1,32 @@
+# 게임월드 모듈
+
+# 게임월드 표현
+world = [[], [], []]
+
+
+# 게임월드에 객체 담기
+def add_object(o, depth=0):
+    world[depth].append(o)
+
+
+# 게임월드 객체들을 몽땅 업데이트
+def update():
+    for layer in world:
+        for o in layer:
+            o.update()
+
+
+# 게임 월드의 객체들을 몽땅 그리기
+def render():
+    for layer in world:
+        for o in layer:
+            o.draw()
+
+
+# 객체 삭제
+def remove_object(o):
+    for layer in world:
+        if o in layer:
+            layer.remove(o)
+            return
+    raise ValueError('object is not in world')
